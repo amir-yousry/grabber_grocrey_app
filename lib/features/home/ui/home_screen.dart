317f8a38f23f4c6fc29ed13f969eba662ca4315d
Amir Yousry <mr.amiryousry@gmail.com>
2026-07-30 17:12:57 +0300

    create  appBar

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:grabber_grocrey_app/features/home/data/items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +10,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
-      )
+        leading: SizedBox.shrink(),
+        leadingWidth: 0,
+        title: Row(
+          children: [
+            SvgPicture.asset("assets/icons/motor.svg"),
+            SizedBox(width: 10),
+            Text("61 Hopper street..", style: TextStyle(fontSize: 19)),
+            SizedBox(width: 10),
+            Icon(Icons.keyboard_arrow_down_rounded, size: 34),
+            Spacer(),
+            SvgPicture.asset("assets/icons/basket.svg"),
+          ],
+        ),
+      ),
     );
   }
 }
