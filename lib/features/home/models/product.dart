04fbd0334de3e3e2419aa19774a3b4750aae87ac
Amir Yousry <mr.amiryousry@gmail.com>
2026-07-30 16:25:31 +0300

    add demo data to test it later

--- lib/features/home/models/product.dart
+++ lib/features/home/models/product.dart
@@ -1,11 +1,11 @@
-class ProductModel {
+class Product {
   final String name;
   final String image;
   final String price;
   final String rate;
   final String rateCount;
 
-  ProductModel({
+  Product({
     required this.name,
     required this.image,
     required this.price,
