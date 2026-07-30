5054e4f4dc35c1f8db299e3726a89e30befd994f
Amir Yousry <mr.amiryousry@gmail.com>
2026-07-30 15:55:18 +0300

    Change default branch from Main to Development (#4)
    * chore: organize project folder structure (#1)
    
    * docs: add README with project overview and screenshots (#2)
    
    * chore: organize project folder structure
    
    * docs: add README with project overview and screenshots
    
    * Design Splash Screen UI (#3)
    
    * chore: organize project folder structure
    
    * docs: add README with project overview and screenshots
    
    * design splash screen ui

---
+++ lib/features/splash/ui/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:grabber_grocrey_app/features/home/ui/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(context, MaterialPageRoute(builder: (c) => HomeScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: SvgPicture.asset('assets/logo/logo.svg')),
+    );
+  }
+}
